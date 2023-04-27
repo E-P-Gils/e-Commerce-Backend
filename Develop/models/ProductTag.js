e3adbf5ce28277630f14281563bae6edc5dbf4f0
unknown <sculptoid11@gmail.com>
2023-04-27 17:20:07 -0400

    Added associations between models in the index.js now adding the routes

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -19,10 +19,10 @@ ProductTag.init(
         key: 'id'
       }
     },
-    product_id: {
+    tag_id: {
       type: DataTypes.INTEGER, 
       references: {
-        model: 'Product',
+        model: 'Tag',
         key: 'id'
       }
     }
