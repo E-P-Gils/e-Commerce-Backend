703ce1696361cedf0ffe1aedb18d0ec446f8927a
unknown <sculptoid11@gmail.com>
2023-04-30 18:10:55 -0400

    Completed!

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -26,7 +26,7 @@ router.get('/:id', async (req, res) => {
 
 
     if (!tagData) {
-      res.status(404).json({ message: 'No Category found with that id!' });
+      res.status(404).json({ message: 'No Tag found with that id!' });
       return;
     }
 
